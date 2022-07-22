cc6cdbe6c23de8dcab11609b111fa61be50344f4
anhdz1102 <96427913+thean2001123@users.noreply.github.com>
2022-07-22 08:27:22 +0700

    update

---
+++ app/login/signup.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.html',
+  styleUrls:['./login.component.css']
+})
+
+export class SignupCPN{
+
+}
