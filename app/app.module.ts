d91a7e45e804fad00043671788971321d98fcc11
anhdz1102 <96427913+thean2001123@users.noreply.github.com>
2022-07-22 08:28:12 +0700

    update

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,19 +5,44 @@ import { AppComponent } from './app.component';
 import {StudentComponent} from "./student.component";
 import {ClassroomComponent} from "./classroom.component";
 import {status} from "./class/class";
+import {CurrentweatherComponent} from "./currentweather/currentweather.component";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {FCWTcomponent} from "./FCweather/fcwt";
+import {ChildFCWT} from "./childFCWT/childFCWT";
+import {LoginComponent} from "./login/login.component";
+import {RouterModule, Routes} from "@angular/router";
+import {SignupCPN} from "./login/signup";
+const rt:Routes=[
+  {path:'signup',component:SignupCPN},
+  {path:'login',component:LoginComponent},
+  {path:'', redirectTo:'login',pathMatch:'full'},
+  {path:'login/signup', redirectTo:'signup',pathMatch:'full'},
+  {path:'signup/login', redirectTo:'login',pathMatch:'full'},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
     ClassroomComponent,
-    status
+    status,
+    CurrentweatherComponent,
+    FCWTcomponent,
+    ChildFCWT,
+    LoginComponent,
+    SignupCPN
+
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(rt)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
